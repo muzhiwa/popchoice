bca8a2bbee9887fe60d537e30dac1031ceb0d72e
muzhiwa <muzhdawafa2008@gmail.com>
2025-11-02 17:20:24 +0430

    Revert "added netlify function"
    This reverts commit 65b6c237ff0e6be234d451daf0e2edfd480e750e.

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,15 @@
+import OpenAI from "openai";
 import { createClient } from "@supabase/supabase-js";
+
+/** OpenAI config */
+if (!process.env.REACT_APP_OPENAI_API_KEY) {
+  throw new Error("Missing REACT_APP_OPENAI_API_KEY in .env");
+}
+
+export const openai = new OpenAI({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
 
 /** Supabase config */
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
